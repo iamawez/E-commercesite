131f8363798a03636832ecafc1ffdf97146e7076
iamawez <awezmanyar1672@gmail.com>
2023-12-28 12:20:10 +0530

    update all

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('veiw'));
 
 app.get('/', (req, res) => {
     console.log("E-commerce site");
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'veiw', 'index.html'));
 });
 
 app.listen(port, () => {
